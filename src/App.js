1107de9b89b4dbd52adcc31f95c61c52b6f72a85
kiberd <34852597+kiberd@users.noreply.github.com>
2021-09-24 18:50:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const COMMIT_HISTORY = gql`
+  query {
+    repository(owner: "kiberd", name: "movie-app") {
+      url
+      createdAt
+      pushedAt
+      defaultBranchRef {
+        target {
+          ... on Commit {
+            history(first: 10) {
+              edges {
+                node {
+                  oid
+                  committedDate
+                  author {
+                    name
+                  }
+                  messageHeadline
+                  commitUrl
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(COMMIT_HISTORY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  // let commits;
+  // if(data){
+  //   commits = data.repository.defaultBranchRef.target.history.edges.node;
+  //   console.log(commits);
+  // }
+  console.log(data.repository.defaultBranchRef.target.history.edges);
+
+  const commits = data.repository.defaultBranchRef.target.history.edges;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {data
+          ? commits.map((commit) => {
+             <div>{commit.node.commitUrl}</div>
+            })
+          : null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+// const client = new ApolloClient({
+//   uri: "https://api.github.com/graphql",
+//   cache: new InMemoryCache(),
+// });
+
+
+
+// Create the http link
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+// Generate and set the header with the auth details
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from env variables if it exists
+  const token = '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';
+
+  // return the headers to the context so httpLink can read them
+  return {
+      headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : "",
+      }
+  }
+});
+
+// Generate your client with the authLink and httpLink
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
+});
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
